03bf75b44ffabea8b19d1b185eaf4a85e9742524
nureesong <76163168+nureesong@users.noreply.github.com>
2022-10-04 20:40:39 +0900

    Add: 5656_벽돌깨기.cpp

--- SWEA/5656_벽돌깨기.cpp
+++ SWEA/5656_벽돌깨기.cpp
@@ -1,11 +1,145 @@
-/************************************/
+/***********************************************************/
 // [5656] 벽돌 깨기
 // DFS - 중복순열, FloodFill & Queue
-// 1921ms, 13560KB
-// 시간초과 해결 -> sol == 0이 되면 더이상 진행할 필요없이 중단!
-// 10:35 ~ 11:35 (1시간)
-/************************************/
+// 1) 1921ms: 가지치기 -> sol == 0이 되는 경우 찾으면 탐색 중단!
+//    10:35 ~ 11:35 (1시간 소요)
+// 2) 33ms: 솔루션 -> 구슬 하나 쏠 때마다 벽돌 제거, 백트래킹 시 복원
+//    - 백업할 배열을 DFS 로컬 변수로 선언해야 2번 이상 백트래킹해서
+//      함수 스택으로 돌아왔을 때 그 이전 상태가 저장되어 있다!!
+//    - memcpy가 copy보다 조금 빠르다.
+//    - 붕뜨는 공간 바닥으로 내릴 때, 큐 보다 while문 돌리는 게 훨씬 빠르다.
+/************************************************************/
 
+// 33ms - 구슬 하나 쏠 때마다 벽돌 제거, 백트래킹 할 때 복원
+#if 1
+#include <iostream>
+#include <queue>
+#include <cstring>
+using namespace std;
+
+int N, R, C;
+int arr[15 + 2][12 + 2];
+
+int M;   // 초기 벽돌 개수
+int sol; // 남은 벽돌 개수 최솟값
+
+struct STATUS {
+	int r, c;
+	int n; // 벽돌에 적힌 숫자
+};
+queue<STATUS> q;
+
+
+void Input() {
+	M = 0;
+	sol = 0x7fffffff;
+
+	cin >> N >> C >> R;
+	for (int i = 0; i < R; i++) {
+		for (int j = 0; j < C; j++) {
+			cin >> arr[i][j];
+			if (arr[i][j]) M++;
+		}
+	}
+}
+
+void Arrange() {
+	for (int j = 0; j < C; j++) {
+		for (int i = R - 1; i >= 0; i--) {
+			if (!arr[i][j]) {
+				int r = i - 1;
+				while (r >= 0 && !arr[r][j]) r--;
+				if (r < 0) break;
+				// r행 값을 i행으로 내리기
+				arr[i][j] = arr[r][j];
+				arr[r][j] = 0;
+			}
+		}
+	}
+}
+
+
+int FloodFill(int r, int c) {
+	static int dr[] = { -1,1,0,0 };
+	static int dc[] = { 0,0,-1,1 };
+
+	q.push({ r,c, arr[r][c] }); // 구슬 명중 위치
+	arr[r][c] = 0;
+	int cnt = 1;
+
+	while (!q.empty()) {
+		STATUS cur = q.front(); q.pop();
+
+		// 4방향으로 n-1칸 범위에 반응하는 벽돌 찾기
+		for (int i = 0; i < 4; i++) {
+			int nr = cur.r;
+			int nc = cur.c;
+			for (int j = 1; j < cur.n; j++) {
+				nr += dr[i];
+				nc += dc[i];
+				if (nr < 0 || nr >= R || nc < 0 || nc >= C) break;
+				if (!arr[nr][nc]) continue;
+
+				int n = arr[nr][nc];
+				if (n > 1) q.push({ nr,nc,n });
+				arr[nr][nc] = 0;  // 벽돌 제거
+				cnt++;
+			}
+		}
+	}
+
+	Arrange();  // 빈 공간 정리
+	return cnt;
+}
+
+
+// n: 지금까지 쏜 구슬 횟수, rem: 남은 벽돌 개수
+void DFS(int n, int rem) {
+	// N번 쏘기 전에 모든 벽돌이 제거될 수도 있다. (n == N일 때만 갱신하면 안 됨!)
+	// 모든 상태가 완성된 경우의 수라고 보고 매번 sol을 갱신한다.
+	if (rem < sol) sol = rem; 
+	
+	if (n == N) return;
+
+	// 현재 상태 백업
+	int cop[15 + 2][12 + 2]; // 전역에 선언하면 두 번 백트래킹 했을 때 복원 안 됨
+	memcpy(cop, arr, sizeof(arr));
+
+	for (int c = 0; c < C; c++) {
+		// 맨 위 벽돌 위치 찾기
+		int r = 0;
+		while (r < R && !arr[r][c]) r++;
+		if (r == R) continue; // c열이 전부 빈칸이면 선택X
+
+		int cnt = FloodFill(r, c); // 제거한 벽돌 개수
+		DFS(n + 1, rem - cnt);
+
+		// 복원
+		memcpy(arr, cop, sizeof(cop));
+	}
+}
+
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+	freopen("in.txt", "r", stdin);
+
+	int T;
+	cin >> T;
+	for (int i = 1; i <= T; i++) {
+		Input();
+		DFS(0, M);
+		cout << "#" << i << " " << sol << "\n";
+	}
+	return 0;
+}
+#endif
+
+
+// 1921ms - 모든 구슬의 위치를 결정한 후, 한번에 벽돌 제거
+#if 0
 #include <iostream>
 #include <algorithm>
 #include <queue>
@@ -41,11 +175,9 @@ void Input() {
 	}
 }
 
-
 void Arrange() {
 	for (int j = 0; j < C; j++) {
 		a = {};
-
 		for (int i = R - 1; i >= 0; i--) {
 			if (cop[i][j]) {
 				a.push(cop[i][j]);
@@ -98,7 +230,6 @@ void FloodFill(int c) {
 		cop[cur.r][cur.c] = 0;
 		rem--;
 	}
-
 	// 빈 공간 벽돌 정리
 	Arrange();
 }
@@ -142,3 +273,4 @@ int main() {
 	}
 	return 0;
 }
+#endif
