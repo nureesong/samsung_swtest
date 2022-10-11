1d2ccbfd577c052c4a32f4731ef80ee3ce49d266
nureesong <76163168+nureesong@users.noreply.github.com>
2022-10-11 14:20:06 +0900

    Mod: 20058_마법사상어와_파이어스톰.cpp

--- c_lang/20058_마법사상어와_파이어스톰.cpp
+++ c_lang/20058_마법사상어와_파이어스톰.cpp
@@ -1,9 +1,15 @@
-/********************************************/
+/******************************************************/
 // [20058] 마법사 상어와 파이어스톰 (골드3)
 // 구현 - 44ms, 2248KB (1시간 3분 소요)
 // A의 subgrid를 회전하여 tmp에 저장 후 A에 복사 
 // tmp는 [0,n) x [0,n)만 사용
-/********************************************/
+//
+// 40ms: A의 테두리를 얼음양 0으로 생각하고 (1,1)부터 시작 
+//       -> 인덱스 범위 체크 필요없어서 4ms 빨라짐
+//       -> FloodFill을 재귀로 구현하면 20ms 까지 가능
+// (sr,sc)에서 시작하는 subgrid를 tmp[sr][sc]에 옮기기 -> 실행시간은 동일
+// tmp[sr + c][sc + (n-1) - r] = A[sr + r][sc + c]
+/******************************************************/
 
 #include <iostream>
 #include <queue>
@@ -11,9 +17,8 @@
 using namespace std;
 
 int N, Q;
-int R, C;
-int A[64][64];   
-int tmp[64][64];
+int A[64 + 2][64 + 2]; // 테두리 얼음양 0으로 생각
+int tmp[64 + 2][64 + 2];
 int L[1000];
 int sumA;
 int dr[] = { -1,1,0,0 };
@@ -28,14 +33,13 @@ queue<POS> q;
 void Input() {
 	cin >> N >> Q;
 	
-	R = C = pow(2, N);
-	for (int i = 0; i < R; i++) {
-		for (int j = 0; j < C; j++) {
+	N = pow(2, N);
+	for (int i = 1; i <= N; i++) {
+		for (int j = 1; j <= N; j++) {
 			cin >> A[i][j];
 			sumA += A[i][j];
 		}
 	}
-	
 	for (int i = 0; i < Q; i++) {
 		cin >> L[i];
 	}
@@ -45,23 +49,15 @@ void Input() {
 void RotateSubgrid(int sr, int sc, int n) {
 	for (int r = 0; r < n; r++) {
 		for (int c = 0; c < n; c++) {
-			tmp[c][n - 1 - r] = A[sr + r][sc + c];
-		}
-	}
-	
-	for (int r = 0; r < n; r++) {
-		for (int c = 0; c < n; c++) {
-			A[sr + r][sc + c] = tmp[r][c];
+			tmp[sr + c][sc + n - 1 - r] = A[sr + r][sc + c];
 		}
 	}
 }
 
-// n = 2^L (step size)
 void Rotate(int n) {
-	if (n == 1) return;
-
-	for (int i = 0; i < R; i += n) {
-		for (int j = 0; j < C; j += n) {
+	// n = 2^L (step size)
+	for (int i = 1; i <= N; i += n) {
+		for (int j = 1; j <= N; j += n) {
 			// (i,j)에서 시작하는 n x n subgrid 를 90도 회전
 			RotateSubgrid(i, j, n);
 		}
@@ -69,26 +65,24 @@ void Rotate(int n) {
 }
 
 void RemoveIce() {
-	vector<POS> rem;
-	for (int r = 0; r < R; r++) {
-		for (int c = 0; c < C; c++) {
-			if (!A[r][c]) continue;
+	for (int r = 1; r <= N; r++) {
+		for (int c = 1; c <= N; c++) {
+			if (!tmp[r][c]) {
+				A[r][c] = 0;
+				continue;
+			}
 
 			int cnt = 0;
 			for (int i = 0; i < 4; i++) {
-				int nr = r + dr[i];
-				int nc = c + dc[i];
-				if (nr < 0 || nr >= R || nc < 0 || nc >= C) continue;
-				if (!A[nr][nc]) continue;
-				cnt++;
+				if (tmp[r + dr[i]][c + dc[i]]) cnt++;
 			}
-			if (cnt < 3) rem.push_back({ r,c });
-		}
-	}
 
-	for (POS p : rem) {
-		A[p.r][p.c]--;
-		sumA--;
+			if (cnt < 3) {
+				A[r][c] = tmp[r][c] - 1;
+				sumA--;
+			}
+			else A[r][c] = tmp[r][c];
+		}
 	}
 }
 
@@ -110,7 +104,6 @@ int BFS(int sr, int sc) {
 		for (int i = 0; i < 4; i++) {
 			int nr = cur.r + dr[i];
 			int nc = cur.c + dc[i];
-			if (nr < 0 || nr >= R || nc < 0 || nc >= C) continue;
 			if (!A[nr][nc]) continue;
 
 			q.push({ nr,nc });
@@ -123,8 +116,8 @@ int BFS(int sr, int sc) {
 
 int FloodFill() {
 	int max = 0;
-	for (int i = 0; i < R; i++) {
-		for (int j = 0; j < C; j++) {
+	for (int i = 1; i <= N; i++) {
+		for (int j = 1; j <= N; j++) {
 			if (!A[i][j]) continue;
 
 			int cnt = BFS(i, j);
