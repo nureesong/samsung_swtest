84ca86b9608fe5ef0bb141b2578f8f3e2565c5cf
nureesong <76163168+nureesong@users.noreply.github.com>
2022-10-12 20:51:06 +0900

    Add: 21608_상어초등학교.cpp

--- c_lang/21608_상어초등학교.cpp
+++ c_lang/21608_상어초등학교.cpp
@@ -2,26 +2,25 @@
 // [21608] 상어 초등학교 (골드5)
 // 구현 - 0ms, 2660KB (2시간 40분 소요)
 // 초기값 설정할 때 반드시 갱신되는지 확인하기!
+// n번 학생이 m번 학생을 좋아하는지 빠르게 판단하기 위해
+// directed 인접행렬을 사용했다. O(1)이지만, 인접행렬이 매우 sparse하다.
+// unordered_set으로 하면 O(logN)이지만, 메모리를 적게 사용한다.
 /*******************************************/
+// 0ms, 2652KB
+// 빈칸 중에서 사방에 좋아하는 학생수, 빈칸수를 동시에 세서
+// 우선순위 조건대로 갱신하면 됨
 
 #include <iostream>
-#include <cstring>
-#include <vector>
 using namespace std;
 
 int N, M;
-int arr[20 + 2][20 + 2];
-int emp[20 + 2][20 + 2];
+int arr[20][20];
 int adj[400 + 1][400 + 1]; // directed
 int S[400];    // 배정 순서대로 학생번호
 
-struct POS {
-	int r, c;
-};
-POS P[400 + 1];
-
 int dr[] = { -1,1,0,0 };
 int dc[] = { 0,0,-1,1 };
+
 
 void Input() {
 	cin >> N;
@@ -37,100 +36,79 @@ void Input() {
 	}
 }
 
-void Init() {
-	emp[1][1] = emp[1][N] = emp[N][1] = emp[N][N] = 2;
-	for (int i = 2; i < N; i++) {
-		emp[i][1] = emp[i][N] = emp[1][i] = emp[N][i] = 3;
-		for (int j = 2; j < N; j++) {
-			emp[i][j] = 4;
-		}
-	}
-}
-
 void Assign(int cur) {
-	int cnt[20 + 2][20 + 2] = { 0, };
+	int maxcnt = -1, maxempty = -1, minr, minc;
 
-	for (int r = 1; r <= N; r++) {
-		for (int c = 1; c <= N; c++) {
+	for (int r = 0; r < N; r++) {
+		for (int c = 0; c < N; c++) {
 			if (arr[r][c]) continue;
 
-			// 비어있는 칸: 좋아하는 학생 몇명 있는지 체크
+			// 좋아하는 학생 수, 빈칸수 동시에 체크
+			int cnt_like = 0, cnt_empty = 0;
+
 			for (int d = 0; d < 4; d++) {
 				int nr = r + dr[d];
 				int nc = c + dc[d];
-				
+				if (nr < 0 || nr >= N || nc < 0 || nc >= N) continue;
+
 				int nbd = arr[nr][nc];
-				if (adj[cur][nbd]) cnt[r][c]++;
+				if (!nbd) cnt_empty++;
+				else if (adj[cur][nbd]) cnt_like++;
+			}
+
+			if (cnt_like > maxcnt ||
+				cnt_like == maxcnt && cnt_empty > maxempty ||
+				cnt_like == maxcnt && cnt_empty == maxempty && r < minr ||
+				cnt_like == maxcnt && cnt_empty == maxempty && r == minr && c < minc) {
+				maxcnt = cnt_like;
+				maxempty = cnt_empty;
+				minr = r;
+				minc = c;
 			}
 		}
 	}
 
-	// 좋아하는 학생이 가장 많은 빈칸 찾기
-	int maxcnt = -1;
-	vector<POS> maxpos;
-	for (int r = 1; r <= N; r++) {
-		for (int c = 1; c <= N; c++) {
-			if (arr[r][c]) continue; // 빈칸만 봐야지!!!
-
-			if (cnt[r][c] > maxcnt) {
-				maxcnt = cnt[r][c];
-				maxpos.clear();
-				maxpos.push_back({ r,c });
-			}
-			else if (cnt[r][c] == maxcnt) {
-				maxpos.push_back({ r,c });
-			}
-		}
-	}
-
-	// 최댓값이 여러 칸이면, 인접 영역이 가장 많이 빈 곳 찾기
-	POS maxP;
-	if (maxpos.size() > 1) {
-		int maxempty = -1; // 주변에 빈칸이 없을수도 있지... 본인만 빈칸이면됐지..
-		for (POS p : maxpos) {
-			if (emp[p.r][p.c] > maxempty) {
-				maxempty = emp[p.r][p.c];
-				maxP = p;
-			}
-		}
-	}
-	else maxP = maxpos[0];
-	
-	arr[maxP.r][maxP.c] = cur;
-	P[cur] = { maxP.r,maxP.c };
-	for (int d = 0; d < 4; d++) {
-		int nr = maxP.r + dr[d];
-		int nc = maxP.c + dc[d];
-		if (emp[nr][nc]) emp[nr][nc]--;
-	}
+	arr[minr][minc] = cur;
 }
 
+
 void Simulation() {
-	int r = 2, c = 2;
-	arr[r][c] = S[0];
-	P[S[0]] = { r,c }; // 0번째 학생은 무조건 (2,2)
-	emp[r - 1][c]--; emp[r + 1][c]--;
-	emp[r][c - 1]--; emp[r][c + 1]--;
+	int r = 1, c = 1;
+	arr[r][c] = S[0];  // 0번째 학생은 무조건 (1,1)
 
 	for (int i = 1; i < M; i++) {
 		Assign(S[i]);
 	}
+
+	for (int r = 0; r < N; r++) {
+		for (int c = 0; c < N; c++) {
+			cout << arr[r][c] << " ";
+		}
+		cout << "\n";
+	}
+	cout << "\n";
 }
 
 int SatisfySum() {
 	int score[] = { 0,1,10,100,1000 };
 	int sum = 0;
-	for (int i = 0; i < M; i++) {
-		int count = 0;
-		int cur = S[i];
-		int r = P[cur].r, c = P[cur].c;
 
-		for (int d = 0; d < 4; d++) {
-			int nbd = arr[r + dr[d]][c + dc[d]];
-			if (adj[cur][nbd]) count++;
+	for (int r = 0; r < N; r++) {
+		for (int c = 0; c < N; c++) {
+			int cur = arr[r][c];
+			int cnt = 0;
+			for (int d = 0; d < 4; d++) {
+				int nr = r + dr[d];
+				int nc = c + dc[d];
+				if (nr < 0 || nr >= N || nc < 0 || nc >= N) continue;
+
+				int nbd = arr[nr][nc];
+				if (adj[cur][nbd]) cnt++;
+			}
+			sum += score[cnt];
 		}
-		sum += score[count];
 	}
+
 	return sum;
 }
 
@@ -141,8 +119,6 @@ int main() {
 	cout.tie(0);
 
 	Input();
-	Init();
-
 	Simulation();
 	cout << SatisfySum() << "\n";
 }
