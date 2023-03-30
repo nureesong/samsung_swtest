b48e2ddd4b9c56575866ddaf297e1b9fca7ad217
nureesong <76163168+nureesong@users.noreply.github.com>
2023-03-30 21:05:54 +0900

    Mod: G4_1976_여행가자.cpp

--- Pro/G4_1976_여행가자.cpp
+++ Pro/G4_1976_여행가자.cpp
@@ -1,7 +1,8 @@
 /***********************************/
 // [1976] 여행가자 (골드4)
 // - 유니온파인드. 재귀호출
-// - 0ms 2316KB
+// - set iterator: 0ms 2180KB
+// - vector: 0ms 2316KB
 /***********************************/
 
 #include <iostream>
@@ -10,10 +11,11 @@
 using namespace std;
 
 int N, M;
-int A[200][200];  // Adjacency matrix (symmetric)
-int root[200];
-set<int> s; // Journey plan (오름차순 정렬)
-vector<int> v; // 중복도시 제거 및 오름차순 정렬한 여행계획
+int A[200][200]; // Adjacency matrix (symmetric)
+int p[200];      // parent node
+set<int> s;      // Journey plan (중복제거,오름차순 정렬)
+// vector<int> v;   
+
 
 void Input() {
     cin >> N >> M;
@@ -27,49 +29,55 @@ void Input() {
         cin >> x;
         s.insert(--x);
     }
-    // Initialize
-    for (int i = 1; i < N; i++) {
-        root[i] = i;
-    }
-    for (int n : s) {
-        v.push_back(n);
-    }
-    s.clear();
+//     for (int n : s) {
+//         v.push_back(n);
+//     }
 }
 
 int find_root(int n) {
-    if (n == root[n]) return n;
-    root[n] = find_root(root[n]);
-    return root[n];
+    if (n == p[n]) return n;
+    return p[n] = find_root(p[n]);
 }
 
 void Union(int a, int b) {
     a = find_root(a);
     b = find_root(b);
-    if (a < b) root[b] = a;
-    else root[a] = b;
+    if (a < b) p[b] = a;
+    else p[a] = b;
 }
 
 string Solve() {
+    // Initialize
+    for (int i = 1; i < N; i++) {
+        p[i] = i;
+    }
+    // Edge Connection
     for (int i = 0; i < N; i++) {
         for (int j = i+1; j < N; j++) { // upper triangular
-            if (A[i][j] && root[i] != root[j]) {
+            if (A[i][j] && p[i] != p[j]) {
                 Union(i,j);
             }
         }
     }
+
     // 여행계획 Query
-    // 현재 포인터가 가리키는 위치의 값과 그 다음 값을 같이 가져와서 isUnion 하고 싶은데
-    // 둘이 같이 가져올 수 있나..?  -> 모르겠다 일단 벡터로 하자!
-    int sz = v.size() - 1;
-    string ans = "YES";
-    for (int i = 0; i < sz; i++) {
-        if (find_root(v[i]) != find_root(v[i+1])) { // <주의> root 배열에 최종 루트노드가 담겨있지 않음!
-            ans = "NO";
-            break;
-        }
+    // 첫번째 도시의 루트노드와 나머지 도시들의 루트노드가 같은지 비교
+    // 1. set iterator
+    set<int>::iterator it = s.begin();
+    int first = find_root(*it++); // 첫번째 도시의 루트노드
+    while (it != s.end() && first == find_root(*it)) {
+        it++;
     }
-    return ans;
+    if (it != s.end()) return "NO";
+    return "YES";
+
+    // 2. vector
+//    int sz = v.size();
+//    int first = find_root(v[0]);
+//    for (int i = 1; i < sz; i++) {
+//        if (find_root(v[i]) != first) return "NO";
+//    }
+//    return "YES";
 }
 
 int main() {
