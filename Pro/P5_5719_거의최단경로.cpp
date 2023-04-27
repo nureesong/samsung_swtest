c26a4f04a215b6c9d8c0bbd9eef7495e09bffe99
nureesong <76163168+nureesong@users.noreply.github.com>
2023-04-27 21:07:22 +0900

    Add: P5_5719_거의최단경로.cpp

--- Pro/P5_5719_거의최단경로.cpp
+++ Pro/P5_5719_거의최단경로.cpp
@@ -1,7 +1,11 @@
-/****************************************/
+/***********************************************/
 // [5719] 거의 최단 경로 (플래5)
-// 25%에서 메모리 초과... PQ에 경로를 전부 넣은 게 문제일듯
-/****************************************/
+// - 다익스트라: 36ms 2444KB
+// 1. 최단경로 탐색 (여러 개 존재 가능)
+// 2. 최단경로 역추적하며 엣지 제거 (인접리스트 노드번호 -1로 변경)
+// 3. 남은 엣지로 다시 최단경로 탐색
+// - 내가 했던 방법: PQ 상태에 이동경로 저장 -> 메모리 초과
+/***********************************************/
 
 #include <iostream>
 #include <vector>
@@ -11,37 +15,26 @@ using namespace std;
 
 int N, M, S, D;
 struct EDGE {
-    int n, d, flag;
-};
-vector<EDGE> A[500]; // Adjacency List
-vector<int> dp;
-vector<vector<int>> shortest_paths;
-
-struct STATUS {
-    int n, d; //string path;
-    vector<int> path;
-};
-struct comp {
-    bool operator()(STATUS &a, STATUS &b) {
-        return a.d > b.d;
-    }
-};
-priority_queue<STATUS, vector<STATUS>, comp> pq;
-struct STATUS2 {
     int n, d;
 };
-struct comp2 {
-    bool operator()(STATUS2 &a, STATUS2 &b) {
+vector<EDGE> A[500]; // Adjacency List
+vector<int> P[500]; // 경로 역추적. prev[도착] = 출발
+vector<int> dp;
+struct comp {
+    bool operator()(EDGE &a, EDGE &b) {
         return a.d > b.d;
     }
 };
-priority_queue<STATUS2, vector<STATUS2>, comp2> pq2;
+priority_queue<EDGE, vector<EDGE>, comp> pq;
+
 
 void Init() {
-    for (int i = 0; i < 500; i++) A[i].clear();
+    for (int i = 0; i < 500; i++) {
+        A[i].clear();
+        P[i].clear();
+    }
     dp.clear();
-    shortest_paths.clear();
-    pq = {}; pq2 = {};
+    pq = {};
 }
 
 int Input() {
@@ -58,79 +51,49 @@ int Input() {
     return 1;
 }
 
-void Dijkstra() {
+int Dijkstra() {
     pq = {};
-    vector<int> init_path{S};
-    pq.push({S, 0, init_path});
-    dp[S] = 0;
-
-    while (!pq.empty()) {
-        STATUS cur = pq.top(); pq.pop();
-//        cout << "\ncur: " << cur.n << " " << cur.d << " " << cur.path << "\n";
-        if (cur.n == D && cur.d == dp[cur.n]) {
-//            cout << "shortest_path\n";
-            shortest_paths.push_back(cur.path);
-            continue;  // 현재 노드가 도착지면 더이상 상태발전X
-        }
-
-        for (auto next : A[cur.n]) {
-            int nd = cur.d + next.d;
-            vector<int> npath = cur.path;
-            if (nd > dp[next.n]) continue;
-
-            npath.push_back(next.n);
-            pq.push({next.n, nd, npath});
-            dp[next.n] = nd;
-//            cout << "[push] " << e.n << " " << nd << " " << npath << "\n";
-        }
-    }
-}
-
-// 최단경로에 해당하는 엣지들 제거
-void Remove() {
-    for (auto path : shortest_paths) {
-        // path = {0,3,6} / {0,1,5,6}
-        for (int i = 0; i < path.size()-1; i++) {
-            int src = path[i];
-            int dest = path[i+1];
-            for (int j = 0; j < A[src].size(); j++) {
-                if (A[src][j].n == dest) {
-                    A[src][j].flag = 1;
-//                    cout << src << " -> " << A[src][j].n << "\n";
-                }
-            }
-        }
-//        cout << "\n";
-    }
-}
-
-int Dijkstra2() {
-    pq2 = {};
-    pq2.push({S, 0});
+    pq.push({S, 0});
     dp.clear();
     dp.resize(N, INF);
     dp[S] = 0;
 
-    while (!pq2.empty()) {
-        STATUS2 cur = pq2.top(); pq2.pop();
-//        cout << "\ncur: " << cur.n << " " << cur.d << "\n";
-        if (cur.n == D && cur.d == dp[cur.n]) {
-            return dp[cur.n];
-        }
+    while (!pq.empty()) {
+        EDGE cur = pq.top(); pq.pop();
+        if (cur.n == D && cur.d == dp[D]) return dp[D]; // 목적지 도착. 최단경로 달성!
 
-        for (auto next : A[cur.n]) {
-            if (next.flag) continue; // 제거된 엣지인지 확인
-//            cout << "next: " << next.n << " " << next.flag << "\n";
+        for (int i = 0; i < A[cur.n].size(); i++) {
+            EDGE next = A[cur.n][i];
+            if (A[cur.n][i].n == -1) continue; // 제거된 엣지 확인
 
             int nd = cur.d + next.d;
-            if (nd >= dp[next.n]) continue;
+            if (nd > dp[next.n] || nd > dp[D]) continue;
+            // 가지치기: 목적지의 현재 누적거리보다 길면 상태발전X
 
-            pq2.push({next.n, nd});
-            dp[next.n] = nd;
-//            cout << "[push] " << next.n << " " << nd << "\n";
+            if (nd < dp[next.n]) {
+                dp[next.n] = nd;
+                pq.push({next.n, nd});
+                P[next.n].clear(); // 최단경로 갱신되었으므로 모두 제거
+            }
+            P[next.n].push_back(cur.n); // 최단경로 추가
         }
     }
     return -1;
+}
+
+// 최단경로 역추적 (재귀)
+void Remove(int dst) {
+    if (dst == S) return;
+
+    for (int src : P[dst]) {
+        for (int i = 0; i < A[src].size(); i++) {
+            if (A[src][i].n == dst) {
+                A[src][i].n = -1; // 최단경로에 포함된 엣지 제거
+                Remove(src);
+                break;
+            }
+        }
+    }
 }
 
 int main() {
@@ -141,8 +104,8 @@ int main() {
     freopen("input.txt", "r", stdin);
 
     while (Input()) {
-        Dijkstra();
-        Remove(); // 제거한 엣지는 flag = 1 표시
-        cout << Dijkstra2() << "\n"; // 거의최단경로 재탐색
+        Dijkstra(); // 최단경로 탐색
+        Remove(D);  // 인접리스트의 노드번호를 -1로 표시하여 엣지 제거 
+        cout << Dijkstra() << "\n";  // "거의 최단경로" 재탐색
     }
 }
